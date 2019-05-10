d315b7c828a6b69153e6355d2ce0375f03021ae6
Rekseto <ReksetoAjxer@gmail.com>
2019-05-11 00:03:57 +0200

    Paginator refactor

--- frontend/src/shared/Paginator/index.jsx
+++ frontend/src/shared/Paginator/index.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import "./index.css";
-export default ({pages, searchQuery}) => {
+export default ({pages, searchQuery, page}) => {
   let query = "";
   if (searchQuery) query = searchQuery;
+  const PaginationItem = (el, index) => {
+    const className =
+      index + 1 == page
+        ? "pagination__link pagination__link--selected"
+        : "pagination__link";
+
+    return (
+      <Link key={index + 1} className={className} to={`${index + 1 + query}`}>
+        {index + 1}
+      </Link>
+    );
+  };
+
   return (
-    <div className="paginator">
-      {[...Array(Math.ceil(pages))].map((el, index) => {
-        return (
-          <Link
-            key={index + 1}
-            className="paginator"
-            to={`${index + 1 + query}`}
-          >
-            {index + 1}
-          </Link>
-        );
-      })}
+    <div className="pagination">
+      {[...Array(Math.ceil(pages))].map(PaginationItem)}
     </div>
   );
 };
